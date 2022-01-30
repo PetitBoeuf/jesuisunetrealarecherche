ce5fa865f7f007c49862ae160f5c4ae099944b6d
PetitBoeuf <marcojl2000@hotmail.com>
2022-01-30 20:25:09 +0100

    merging on the other side?

---
+++ 1.py
@@ -0,0 +1,28 @@
+import csv
+import operator
+
+data = []; indata = []
+
+with open('data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    next(spamreader)
+    for row in spamreader:
+        indata = [] 
+        indata.append(row[0])
+        for index in range(1,3):
+            indata.append(int(row[index]))
+        data.append(indata)
+
+data = sorted(data, key=operator.itemgetter(1))
+
+data_sum=0
+total_duration=0
+for row in data:
+    data_sum = row[1] + data_sum
+    total_duration = total_duration + data_sum
+
+average_time = total_duration / len(data)
+
+print(data)
+print(average_time)
+print(data_sum)
---
+++ 2v1.py
@@ -0,0 +1,32 @@
+import csv
+import operator
+
+data = []; indata = []
+
+with open('data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    next(spamreader)
+    for row in spamreader:
+        indata = [] 
+        indata.append(row[0])
+        for index in range(1,3):
+            indata.append(int(row[index]))
+        data.append(indata)
+
+data = sorted(data, key=operator.itemgetter(2))
+
+data_sum=0
+total_duration=0
+retard_accumule=0
+for row in data:
+    data_sum += row[1]
+    total_duration += data_sum
+    if retard_accumule < data_sum - row[2]:
+        retard_accumule = data_sum - row[2]
+
+average_time = total_duration / len(data)
+print(data)
+#print(average_time)
+print(data_sum)
+print(total_duration)
+print(retard_accumule)
